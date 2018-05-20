f13338e5796773ac261880c0e79152c92bc4c10c
Sergey Nechaev <serg.nechaev@gmail.com>
2018-05-21 00:18:26 +1000

    wallet re-factoring

--- io.zdp.wallet.desktop.ui/package.bat
+++
@@ -1,7 +0,0 @@
-set JRE_HOME=C:\tools\Java\jdk1.8.0_45\jre
-set JAVA_HOME=C:\tools\Java\jdk1.8.0_45
-set PATH=%PATH%;C:\tools\Java\jdk8.0_45\bin;
-
-set MAVEN_OPTS=-Xmx4G
-rem C:/tools/maven3.0.3/bin/mvn install -DskipTests=true
-C:/tools/maven3.2.2/bin/mvn clean install package
