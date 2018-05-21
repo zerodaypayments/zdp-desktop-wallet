899881396e43a5dc01c5d6b9f36b0fd654f41c03
Sergey Nechaev <serg.nechaev@gmail.com>
2018-05-21 23:33:46 +1000

    wallet updates

--- io.zdp.wallet.desktop.client/src/main/java/io/zdp/wallet/desktop/ui/gui/action/CreateNewWallet.java
+++ io.zdp.wallet.desktop.client/src/main/java/io/zdp/wallet/desktop/ui/gui/action/CreateNewWallet.java
@@ -2,36 +2,26 @@ package io.zdp.wallet.desktop.ui.gui.action;
 
 import java.awt.Window;
 import java.io.File;
-import java.util.List;
 
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
 import org.bouncycastle.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.zdp.crypto.Curves;
-import io.zdp.crypto.key.ZDPKeyPair;
-import io.zdp.crypto.mnemonics.Mnemonics;
-import io.zdp.crypto.mnemonics.Mnemonics.Language;
 import io.zdp.wallet.api.db.domain.Wallet;
 import io.zdp.wallet.desktop.ui.common.Alert;
 import io.zdp.wallet.desktop.ui.common.I18n;
-import io.zdp.wallet.desktop.ui.common.QTextComponentContextMenu;
 import io.zdp.wallet.desktop.ui.common.SwingHelper;
 import io.zdp.wallet.desktop.ui.common.SynchronousJFXFileChooser;
-import io.zdp.wallet.desktop.ui.common.TextComponentFocuser;
 import io.zdp.wallet.desktop.ui.common.model.BooleanWrapper;
 import io.zdp.wallet.desktop.ui.common.model.StringWrapper;
 import io.zdp.wallet.desktop.ui.gui.MainWindow;
 import io.zdp.wallet.desktop.ui.gui.dialog.PasswordPanel;
-import io.zdp.wallet.desktop.ui.gui.dialog.WalletCreationPanel;
 import io.zdp.wallet.desktop.ui.service.DesktopWalletService;
 import javafx.application.Platform;
 import javafx.stage.FileChooser;
@@ -50,7 +40,7 @@ public class CreateNewWallet {
 	@Autowired
 	private MainWindow mainWindow;
 
-	public void create( Window parent ) {
+	public void create(Window parent) {
 
 		// Enter DB file password for AES encryption
 		String password = enterPassword(parent);
@@ -66,6 +56,7 @@ public class CreateNewWallet {
 		Platform.setImplicitExit(false);
 
 		SynchronousJFXFileChooser chooser = new SynchronousJFXFileChooser(() -> {
+
 			FileChooser ch = new FileChooser();
 
 			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("ZDP Wallet (*.zdp)", "*.zdp");
@@ -76,21 +67,19 @@ public class CreateNewWallet {
 			return ch;
 		});
 
-		File walletFile = chooser.showOpenDialog();
+		File walletFile = chooser.showSaveDialog();
 
-		if (walletFile == null || false == walletFile.canWrite()) {
+		log.debug("walletFile: " + walletFile);
+
+		if (walletFile == null) {
 			return;
 		}
-/*
-		newWalletDialog.dispose();
 
 		log.debug("Save new wallet: " + walletFile);
 
 		try {
 
-			String seed = panel.txtSeed.getText();
-
-			Wallet w = walletService.create(seed, walletFile);
+			Wallet w = walletService.create(password, walletFile);
 
 			walletService.setCurrentWallet(w, walletFile);
 
@@ -105,10 +94,10 @@ public class CreateNewWallet {
 		} catch (Exception e) {
 			log.error("Error: ", e);
 		}
-*/
+
 	}
 
-	private String enterPassword( Window parent ) {
+	private String enterPassword(Window parent) {
 
 		StringWrapper password = new StringWrapper();
 
@@ -124,12 +113,11 @@ public class CreateNewWallet {
 			pp.btnOk.setEnabled(true);
 
 			pp.btnOk.addActionListener(e -> {
-				
-				
+
 				if (pp.password.getPassword().length == 0 || pp.passwordConfirm.getPassword().length == 0) {
-					
+
 					JOptionPane.showMessageDialog(parent, "Please, enter passwords", "Warning", JOptionPane.WARNING_MESSAGE);
-					
+
 				} else if (pp.password.getPassword().length > 0 && Arrays.areEqual(pp.password.getPassword(), pp.passwordConfirm.getPassword())) {
 
 					password.set(new String(pp.password.getPassword()));
